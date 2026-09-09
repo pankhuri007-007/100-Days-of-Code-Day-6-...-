dc16c8eaa24574f457a8c4ab3251be06cafc3792
pankhuri007-007 <pankhurikhanna71@gmail.com>
2026-09-09 17:40:29 +0530

    Added Day 6 Q12 program

---
+++ day6Q12.c
@@ -0,0 +1,23 @@
+//Q12 (Conditional Statements)
+//Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    if (n >= 0) {
+        if (n == 0)
+            printf("Zero");
+        else
+            printf("Positive");
+    }
+    else {
+        printf("Negative");
+    }
+
+    return 0;
+}
+
