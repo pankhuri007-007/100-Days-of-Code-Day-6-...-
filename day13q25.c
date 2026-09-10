10767692fe91c45d318cf620f34bd041da295ea9
pankhuri007-007 <pankhurikhanna71@gmail.com>
2026-09-10 16:25:23 +0530

    Day 13 completed

---
+++ day13q25.c
@@ -0,0 +1,39 @@
+//Day 13
+//Q25 (Conditional Statements)
+//Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch(op) {
+        case '+':
+            printf("%d", a + b);
+            break;
+
+        case '-':
+            printf("%d", a - b);
+            break;
+
+        case '*':
+            printf("%d", a * b);
+            break;
+
+        case '/':
+            printf("%d", a / b);
+            break;
+
+        case '%':
+            printf("%d", a % b);
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
