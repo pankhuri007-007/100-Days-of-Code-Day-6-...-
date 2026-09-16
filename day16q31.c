6ca1328750915c7fdd8628d1ee7923c4a891af47
pankhuri007-007 <pankhurikhanna71@gmail.com>
2026-09-16 16:34:25 +0530

    Add files via upload

---
+++ day16q31.c
@@ -0,0 +1,28 @@
+//Day 16
+//Q31 (Loops without Arrays/Strings)
+
+#include <stdio.h>
+
+int main() {
+    int n, binary[32], i = 0;
+
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("0");
+    } else {
+        while (n > 0) {
+            binary[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
+
+        for (i = i - 1; i >= 0; i--) {
+            printf("%d", binary[i]);
+        }
+    }
+
+    return 0;
+}
+
+
