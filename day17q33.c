47419dfc77c20c375120d7e272aed124cd8c86b1
pankhuri007-007 <pankhurikhanna71@gmail.com>
2026-09-16 16:40:23 +0530

    Add files via upload

---
+++ day17q33.c
@@ -0,0 +1,25 @@
+//Day 17
+//Q33 (Loops without Arrays/Strings)
+//Write a program to check if a number is an Armstrong number.
+
+#include <stdio.h>
+
+int main() {
+    int n, original, digit, sum = 0;
+
+    scanf("%d", &n);
+    original = n;
+
+    while (n > 0) {
+        digit = n % 10;
+        sum = sum + digit * digit * digit;
+        n = n / 10;
+    }
+
+    if (sum == original)
+        printf("Armstrong");
+    else
+        printf("Not Armstrong");
+
+    return 0;
+}
